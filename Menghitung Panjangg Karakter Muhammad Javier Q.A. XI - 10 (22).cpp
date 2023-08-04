0ff904384b94a0cfe8531bbd9b2581c908331706
javieragata <126043887+javieragata@users.noreply.github.com>
2023-08-04 10:21:25 +0700

    Menghitung Panjang Karakter PART 2 BY BANG JEJE
    OKELAH

---
+++ Menghitung Panjangg Karakter Muhammad Javier Q.A. XI - 10 (22).cpp
@@ -0,0 +1,28 @@
+#include <cstdlib>
+#include <iostream>
+#include <cstring>
+using namespace std;
+int main ()
+
+ //mendeklarasi kan teks1 dan teks2
+char teks1 [10], teks2 [10];
+int jlhdepan, jlhkel, jlhhuruf;
+
+ 1/menampilkan pesan u/ meminta masukan dari pengguna
+ cout << "Masukkan Nama Depan Anda?: ":
+ cin >> teks1;
+ Cout << Masukkan Nama Keluarga Anda?:
+ cin >> teks2;
+ 1/menghitung panjang teks
+ jlhdepan = strlen (teksl) ;
+ jlhkel strlen (teks2) ;
+ jlhhuruf = jlhdepan + jlhkel:
+
+ //menampilkan hasil ke pengguna teks2 <<
+ cout << "Nama Lengkap Anda: << teks1 <<
+ endl;
+ cout << "Jumlah Huruf Nama Anda << jlhhuruf <« Huruf"
+ <Kendl;
+ system ("PAUSE");
+ return 0;
+
